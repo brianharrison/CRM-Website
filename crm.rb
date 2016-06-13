16706fe9f8c34a7bfa65a68c9f773de389bf2b02
Brian Harrison <brianharrison@rogers.com>
2016-06-13 16:29:30 -0400

    Completed forms and linked edit contact

---
+++ crm.rb
@@ -0,0 +1,73 @@
+require_relative 'contact'
+require 'sinatra'
+
+Contact.create('Johnny', 'Bravo', 'johnny@bitmakerlabs.com', 'Rockstar')
+Contact.create('Bobby', 'Bravo', 'bobby@bitmakerlabs.com', 'Rockstar 2')
+
+get '/' do
+	@crm_app_name = "Brian's CRM"
+  erb :index
+end
+
+get '/contacts' do
+
+	erb :contacts
+
+end
+
+get '/contacts/new' do
+
+	erb :new_contact 
+end
+
+post '/contacts' do
+	
+	Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+	redirect to('/contacts/new')
+end
+
+get '/contacts/:id' do
+
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+	@contact = Contact.find(params[:id].to_i)
+	if @contact	
+  	erb :edit_contact
+  else
+  	raise Sinatra::NotFound	
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
